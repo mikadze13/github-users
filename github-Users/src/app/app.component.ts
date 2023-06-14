b633db123851f1d9bd3c90696e213f7dc79dd899
Vazha Mikadze <74679350+mikadze13@users.noreply.github.com>
2023-06-14 23:13:16 +0400

    added search

--- github-Users/src/app/app.component.ts
+++ github-Users/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './core/service/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +10,27 @@ import { UserService } from './core/service/user.service';
 export class AppComponent implements OnInit {
   title = 'github-Users';
   userArray: any[] = []
-
-
+  inputValue!: string;
+  private subscription!: Subscription;
   constructor(private user: UserService) { }
 
   ngOnInit(): void {
-    this.user.getUser().subscribe((response) => {
-      response.map((follower: any) => {
-        this.userArray.push(follower) 
-      })
 
-      console.log(this.userArray)
+  }
+  submitForm() {
+    this.user.processInputValue(this.inputValue);
+    // this.subscription = this.user.getUser().subscribe((response) => {
+    //   response.map((follower: any) => {
+    //     this.userArray.push(follower)
+    //   })
+    //   console.log(this.userArray)
+    // })
+    this.subscription  = this.user.getUser().subscribe((response)=>{
+      this.userArray.push(response)
     })
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
