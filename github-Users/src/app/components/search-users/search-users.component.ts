d2b72d7feeabf8c3b85ae6dd97c23c47a7c960a2
Vazha Mikadze <74679350+mikadze13@users.noreply.github.com>
2023-06-18 15:12:19 +0400

    added responsive

--- github-Users/src/app/components/search-users/search-users.component.ts
+++ github-Users/src/app/components/search-users/search-users.component.ts
@@ -9,28 +9,28 @@ import { UserService } from 'src/app/core/service/user.service';
 export class SearchUsersComponent implements OnInit {
   userArray: any = [
     {
-      avatar_url:"https://avatars.githubusercontent.com/u/583231?v=4",
-      name:"The Octocat",
-      created_at:"2011-01-25T18:44:36Z",
-      bio:null,
-      public_repos:8,
-      followers:9515,
-      following:9,
-      location:"San Francisco",
-      twitter_username:"Not available",
-      blog:"https://github.blog",
-      company:"@github",
+      avatar_url: "https://avatars.githubusercontent.com/u/583231?v=4",
+      name: "The Octocat",
+      created_at: "2011-01-25T18:44:36Z",
+      bio: null,
+      public_repos: 8,
+      followers: 9515,
+      following: 9,
+      location: "San Francisco",
+      twitter_username: "Not available",
+      blog: "https://github.blog",
+      company: "@github",
       login: "octocat"
     }
   ]
-  mode:boolean = true
+  mode: boolean = true
   inputValue!: string;
-  light:boolean = false;
-  dark:boolean = true;
-  styleExp!:string
-  styleExpCont!:string
-  styleExpText!:string
-  applyPlaceholderColor:boolean = false
+  light: boolean = false;
+  dark: boolean = true;
+  styleExp!: string
+  styleExpCont!: string
+  styleExpText!: string
+  applyPlaceholderColor: boolean = false 
   constructor(private user: UserService) { }
 
 
@@ -41,15 +41,6 @@ export class SearchUsersComponent implements OnInit {
   // submit form
   submitForm() {
     this.user.processInputValue(this.inputValue);
-    // get user followers
-    // this.user.getUser().subscribe((response) => {
-    //   this.userArray = []
-    //   response.map((follower: any) => {
-
-    //     this.userArray.push(follower)
-    //   })
-    //   console.log(this.userArray)
-    // })
     // get only user
     this.user.getUser().subscribe((response) => {
       this.userArray = []
@@ -59,23 +50,23 @@ export class SearchUsersComponent implements OnInit {
   }
 
   // dark and light mode 
-  lightMode(){
+  lightMode() {
     this.dark = true;
     this.light = false;
     document.body.style.background = 'white'
     this.styleExp = "white"
     this.styleExpCont = "white"
-    this.styleExpText = "#4b6a9b"
+    this.styleExpText = "#4b6a9b" 
   }
 
-  darkMode(){
+  darkMode() {
     this.dark = false;
     this.light = true;
     document.body.style.background = '#141d2f'
-    this.styleExp = "#1e2a47" 
+    this.styleExp = "#1e2a47"
     this.styleExpCont = "#141d2f"
     this.applyPlaceholderColor = true
-    this.styleExpText = "white"
+    this.styleExpText = "white" 
   }
 
 }
